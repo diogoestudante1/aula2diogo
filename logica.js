f87c4b99b15fa04b476578a62f1ef83044ae8db0
diogoestudante1 <diogo.informaticaweb@gmail.com>
2021-11-09 19:11:38 -0300

    adicionando arquivos

---
+++ logica.js
@@ -0,0 +1,16 @@
+const input = require ("readline-sync")
+
+
+const a = "1.Música" 
+b = "2.Livro"
+c = "3.Fruta" 
+d = "4.Violão" 
+e = "5.Bateria"; 
+
+
+console.log(a)
+
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
