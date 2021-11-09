be3607c92dd5745ce0e366514de7fd8f058b8d72
diogoestudante1 <diogo.informaticaweb@gmail.com>
2021-11-09 19:05:39 -0300

    Inserir arquivo para o github

---
+++ chat.js
@@ -0,0 +1,12 @@
+
+const input = require ("readline-sync")
+
+console.log("Olá");
+const idade = input.question ("Qual sua idade?")
+
+if ( idade >=18 && idade <21){" Você pode entrar, mas não pode beber"
+  console.log
+  else if (idade >=21) console.log{("Pode entrar e pode beber")}
+  else { console.log}("Não pode entrar")}
+};
+
